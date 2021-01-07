4564b79063acc6f2cab36c02527a862f4914f146
StoneShiShang <43474457+Stoneshishang@users.noreply.github.com>
2021-01-07 14:10:12 -0500

    how to use jest.mock() to mock API response for testing

--- util.test.js
+++ util.test.js
@@ -1,7 +1,10 @@
+// jest.mock('./http');
+
 const {loadTitle} = require('./util')
 
 test('should print an uppercase text', ()=>{
   loadTitle().then(title =>{
+    console.log('mocking API');
     expect(title).toBe('DELECTUS AUT AUTEM')
   })
 })
